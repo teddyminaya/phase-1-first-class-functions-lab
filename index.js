298cfbfb4c450482d52ed6068a402a699cdefdb8
Teddy Minaya <teddyminaya@gmail.com>
2022-12-14 21:02:43 -0500

    commit tree 4ba1b13ad2c860b43ec1d185a4333a26e720778f

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice (2, 4);
+  }
